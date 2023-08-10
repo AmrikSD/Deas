238bc7cffbb528965e58a920def34020a9389d3c
AmrikSD <amrik.singh@live.co.uk>
2023-08-10 06:32:08 +0000

    Add hacky SQL for accounts

--- app/src/main/java/daes/app/App.java
+++ app/src/main/java/daes/app/App.java
@@ -1,8 +1,31 @@
 package daes.app;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static daes.utilities.sql.Account.insertAccount;
+import daes.utilities.sql.Account.AccountType;
+
 public class App {
 
     public static void main(String[] args) {
+
+
+        String userName = "postgres";
+        String password = "mysecretpassword";
+        String url = new StringBuilder("jdbc:postgresql://localhost:5432/postgres").toString();
+
+        try(Connection conn = DriverManager.getConnection(url, userName, password)){
+
+           int inserted = insertAccount(conn, "Barclays", AccountType.INCOME);
+            System.out.println(inserted);
+
+            
+        } catch (SQLException e){
+            System.out.println(e);
+        }
+
         System.out.println("Hello!");
     }
 
---
+++ utilities/src/main/java/daes/utilities/sql/Account.java
@@ -0,0 +1,33 @@
+package daes.utilities.sql;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Account {
+    
+    public enum AccountType {
+        ASSET,
+        LIABILITY,
+        EQUITY,
+        INCOME,
+        EXPENSE
+    }
+
+    public static int insertAccount(Connection connection, String name, AccountType type) throws SQLException {
+        Statement stmt = connection.createStatement();
+
+        String accountTypeString = type.toString().substring(0, 1).toUpperCase() + type.toString().substring(1).toLowerCase();
+
+        String sqlInsert = String.format("""
+                    INSERT INTO account (type, name)
+                    VALUES (
+                        '%s',
+                        '%s'
+                    );
+                """, 
+                accountTypeString,
+                name);
+        return stmt.executeUpdate(sqlInsert);
+    }
+}
