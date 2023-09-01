22267643b62a2c567348373f31b680b570c8181a
AmrikSD <amrik.singh@live.co.uk>
2023-09-01 06:01:49 +0100

    add some more unit tests for the giggles

--- parser/src/test/java/deas/parser/ParserTest.java
+++ parser/src/test/java/deas/parser/ParserTest.java
@@ -1,14 +1,42 @@
 package deas.parser;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.ParameterizedTest;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ParserTest {
-   @Test void testBasicNumericalValue(){
-    ASTNode expected = new ASTNode(42);
+
+   @DisplayName("Should pass through basic integers to the AST")
+   @ParameterizedTest
+   @CsvSource({"1,1", "3,3", "5,5", "-3,-3","7777777,7777777", "15,15", "2147483647,2147483647"})
+   void testBasicNumericalValue(int number, String string){
+    ASTNode expected = new ASTNode(number);
 
     Parser parser = new Parser();
-    ASTNode actual = parser.parse("42");
+    ASTNode actual = parser.parse(string);
+
+    assertEquals(expected.value, actual.value);
+   }
+
+   @DisplayName("Should pass through Java Integer.MAX_VALUE integer to the AST")
+   void testMaxIntNumericalValue(){
+
+    ASTNode expected = new ASTNode(Integer.MAX_VALUE);
+
+    Parser parser = new Parser();
+    ASTNode actual = parser.parse("2147483647");
+
+    assertEquals(expected.value, actual.value);
+   }
+
+   @DisplayName("VjjShould pass through Java Integer.MIN_VALUE to the AST")
+   void testMinIntNumericalValue(){
+
+    ASTNode expected = new ASTNode(Integer.MIN_VALUE);
+
+    Parser parser = new Parser();
+    ASTNode actual = parser.parse("-2147483648");
 
     assertEquals(expected.value, actual.value);
    }
